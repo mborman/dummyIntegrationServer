6799ee842b4e7c15b252e70f0573387fc659b4be
Mike Borman <mikewborman@gmail.com>
2015-09-03 15:24:45 -0500

    Updates to allow a response to be selected by passing in a "transactionId". Specific need for implementation but could be made to be generally useful.

---
+++ responseBody/index.js
@@ -0,0 +1,38 @@
+
+/**
+ * set the responseBody JSON here. each response is named with a transactionId.
+ * i.e. when a user send a query string with transactionId=good the response
+ * under "good" will be returned
+ */
+
+module.exports = {
+
+  good: {
+    companyId: "ResaleWeekly",
+    itemId: "259853",
+    itemUrl: "http:\/\/www.resaleweekly.com\/catalog\/product\/view\/id\/259853",
+    mobileItemUrl: "http:\/\/www.resaleweekly.com\/catalog\/product\/view\/id\/259853",
+    transactionId: "9b2797f7b1a3bc4b27b85b0cfe610eca",
+    currencyCode: "GBP",
+    description: "IN VERY GOOD WORKING CONDITION NEW DASH MONITOR BEING FITTED NEW TRACK SPADS BEING FITTEDNEW IDLERS BEING FITTED FULLY SERVICED LARGE ROCK BUCKETTRACK CHAINS 80% SPROCKETS 80% NO CRACKS OR WELDING ON BOOM OR DIPPER FIXED FRAME UNDER CARRIAGE MACHINE WHEN WE ARE FINISHED WILL BE AS GOOD AS NEW AND READY FOR WORK ",
+    imageUrl: "http:\/\/www.resaleweekly.com\/media\/catalog\/product\/1\/4\/14c146d681087b6_1408116116.jpg",
+    listPrice: 55000.00,
+    minimumPrice: 0,
+    terms: "",
+    title: "CATERPILLAR 375L"
+  },
+  bad :{
+    companyId: null,
+    //itemId: "259853",
+    itemUrl: "http:\/\/www.resaleweekly.com\/catalog\/product\/view\/id\/259853",
+    mobileItemUrl: "http:\/\/www.resaleweekly.com\/catalog\/product\/view\/id\/259853",
+    transactionId: "9b2797f7b1a3bc4b27b85b0cfe610eca",
+    //currencyCode: "GBP",
+    description: "IN VERY GOOD WORKING CONDITION NEW DASH MONITOR BEING FITTED NEW TRACK SPADS BEING FITTEDNEW IDLERS BEING FITTED FULLY SERVICED LARGE ROCK BUCKETTRACK CHAINS 80% SPROCKETS 80% NO CRACKS OR WELDING ON BOOM OR DIPPER FIXED FRAME UNDER CARRIAGE MACHINE WHEN WE ARE FINISHED WILL BE AS GOOD AS NEW AND READY FOR WORK ",
+    imageUrl: "http:\/\/www.resaleweekly.com\/media\/catalog\/product\/1\/4\/14c146d681087b6_1408116116.jpg",
+    listPrice: 55000.00,
+    minimumPrice: 0,
+    terms: "",
+    title: "CATERPILLAR 375L"
+  }
+};
--- server.js
+++ server.js
@@ -6,8 +6,16 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// ability to parse JSON
 
+// Your own super cool function
+var logger = function(req, res, next) {
+  console.log("GOT REQUEST !");
+  next(); // Passing the request to the next handler in the stack.
+};
+
+app.use(logger);
+
+// ability to parse JSON
 app.use(bodyParser.json());
 
 app.all('*', function (req, res) {
@@ -21,17 +29,31 @@ app.all('*', function (req, res) {
     }
   });
 
-  // make a pretty message
-  var out = req.method + " " + req.originalUrl + "\n\n- - - Headers - - -\n" + 
-    headers + "\n- - - Body - - -\n" + JSON.stringify(req.body, null, 2);
+  // make a pretty message with request info
+  var out = req.method + " " + req.originalUrl + "\n\n- - - Request Headers - - -\n" + 
+    headers + "\n- - - Request Body - - -\n" + JSON.stringify(req.body, null, 2);
   
   // send message to client
-  res.send("Here's what I got...\n\n" + out);
-
+  var responseBody = require('./responseBody');
+  var transactionId = req.query.transactionId || 'good';
+  
+  var body = responseBody[transactionId];
+  var responseHeaders = {
+    "Cache-Control": "no-store, no-cache, must-revalidate, post-check=0, pre-check=0",
+    "Content-Type": "application/json; charset=utf-8",
+    "Expires": "Thu, 19 Nov 1981 08:52:00 GMT"
+  };
+  
+  res.set(responseHeaders);
+  
+  res.send(body);
+  
   // log message to console
   console.log("\n*** " + new Date().toString() + " ***\n\n" + out);
 
 });
+
+
 
 var server = app.listen(8888, function () {
 
@@ -39,6 +61,5 @@ var server = app.listen(8888, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-
-
+  
 });
